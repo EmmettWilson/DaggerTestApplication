dec73a42acacc62cbddf25e3c06c24a4a5a9b478
Emmett Wilson <Mathematicalfunk@gmail.com>
2016-09-12 10:11:09 -0500

    Persist the component accross config change to ensure we get the correct perActivity scoping accross config change.

--- app/src/main/java/com/mathematicalfunk/daggertest/MainActivity.java
+++ app/src/main/java/com/mathematicalfunk/daggertest/MainActivity.java
@@ -9,14 +9,33 @@ import javax.inject.Inject;
 public class MainActivity extends AppCompatActivity {
 
     @Inject StringProvider stringProvider;
+    private ActivityComponent component;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ((DaggerApp) getApplication()).getApplicationComponent().plus(new ActivityModule()).inject(this);
+        inject();
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.hello_textview);
         textView.setText(stringProvider.getString());
+    }
+
+    @Override
+    public ActivityComponent onRetainCustomNonConfigurationInstance() {
+        return component;
+    }
+
+    @Override
+    public ActivityComponent getLastCustomNonConfigurationInstance() {
+        return (ActivityComponent)super.getLastCustomNonConfigurationInstance();
+    }
+
+    private void inject() {
+        component = getLastCustomNonConfigurationInstance();
+        if (component == null) {
+            component =  ((DaggerApp) getApplication()).getApplicationComponent().plus(new ActivityModule());
+        }
+        component.inject(this);
     }
 }
